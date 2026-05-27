60103e8dc5d33402d96773e7e0ea27ae9585f7f5
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-05-27 23:45:34 +0200

    snack 3 + bonus

--- src/main.ts
+++ src/main.ts
@@ -62,3 +62,35 @@ const dipendente: Dipendente = {
   emailAziendale: "amsdkaod@gmail.com",
   contratto: "freelance",
 };
+
+// 🏆 Snack 3
+// Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+type Developer = Dipendente & {
+  // livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+  livelloEsperienza: "Junior" | "Mid" | "Senior";
+  // linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+  linguaggi?: string[];
+  // certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+  certificazione: string[];
+};
+type ProjectManager = Dipendente & {
+  // teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+  teamSize: null | number;
+  // stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
+  stakeholderPrincipali: string[];
+  // budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+  budgetGestito?: number;
+};
+
+// 🎯 BONUS
+// Definiamo un nuovo type alias Team, che rappresenta un gruppo di lavoro all'interno dell'azienda:
+// nome → Nome del team (stringa).
+// progettoAttuale → Nome del progetto su cui lavora il team (può essere null se il team è in attesa di assegnazione).
+// budget → Importo numerico del budget assegnato al team (sempre presente).
+// membri → Una tuple in cui il primo elemento è sempre un Project Manager, seguito da uno o più Developers (almeno un developer obbligatorio).
+type Team = {
+  nome: string;
+  progettoAttuale: null | string;
+  budget: number;
+  membri: [ProjectManager, Developer, ...Developer[]];
+};
